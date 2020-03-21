599eb1039bf941f002f2f9f9834fa2e70801e298
fabian lopez <cflopezm@unal.edu.co>
2020-03-21 17:29:51 -0500

    Mi primer commit

---
+++ inversor.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 21 15:21:01 2020
+
+@author: Fabian Lopez
+"""
+
+print("Este programa invierte las cifras de un numero entero")
+
+num = input("Escribe un digito: ")
+numInv = ""
+
+for i in range(len(num)):
+    k = i+1
+    numInv = numInv+num[-k]
+    
+print(numInv)
+    
---
+++ numDigitos.py
@@ -0,0 +1,9 @@
+print("\nEste programa cuenta el numero de digitos de un entero")
+
+#num = int(input("Escribe aqui un numero entero: "))
+num = "123453423423423435434234"
+
+for i in range(len(num)):
+    i = i+1
+
+print(f"El numero {num} tiene {i} digitos")
